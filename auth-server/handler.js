4559fcc16d59210058cfe21e5cb460eee2c6e23f
stefleon33 <stefleon33@gmail.com>
2024-07-16 15:44:24 -0400

    Updated web address

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,7 +5,7 @@ const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-  "https://YOUR_GITHUB_USERNAME.github.io/meet/"
+  "https://stefleon33.github.io/meet/"
 ];
 
 const oAuth2Client = new google.auth.OAuth2(
