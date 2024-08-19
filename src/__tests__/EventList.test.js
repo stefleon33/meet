9fff8292a8a2fca3b2b9f43266197e45ba6ad808
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-08-18 20:42:05 -0400

    EventList test added

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable testing-library/prefer-presence-queries */
 /* eslint-disable testing-library/prefer-screen-queries */
 /* eslint-disable testing-library/render-result-naming-convention */
+
 // src/__tests__/EventList.test.js
 
 import { render } from '@testing-library/react';
@@ -10,5 +11,11 @@ describe('<EventList /> component', () => {
   test('has an element with "list" role', () => {
     const EventListComponent = render(<EventList />);
     expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+  });
+  test('renders correct number of events', () => {
+    const EventListComponent = render(<EventList events={
+      [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
+    } />);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
   });
 });
