d70d62a4dc5d98c5ecc302ae6c95e92a7e7f9cb2
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-08-19 12:46:59 -0400

    Initial code for CitySearch.test.js

---
+++ src/__tests__/CitySearch.test.js
@@ -0,0 +1,13 @@
+// src/__tests__/CitySearch.test.js
+
+import { render } from '@testing-library/react';
+import CitySearch from '../components/CitySearch';
+
+describe('<CitySearch /> component', () => {
+  test('renders text input', () => {
+    const CitySearchComponent = render(<CitySearch />);
+    const cityTextBox = CitySearchComponent.queryByRole('textbox');
+    expect(cityTextBox).toBeInTheDocument();
+    expect(cityTextBox).toHaveClass('city');
+  });
+});
