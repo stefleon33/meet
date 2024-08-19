5268a0841e0a2150c2e75caae2425515beea276d
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-08-18 20:42:20 -0400

    imported CitySearch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // src/App.js
 
+import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="App">
-      <EventList />
-    </div>
-  );
+ return (
+   <div className="App">
+     <CitySearch />
+     <EventList />
+   </div>
+ );
 }
 
 export default App;
