0243dd60a093902c9a2efb95d21663fb07e54085
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-08-19 12:53:42 -0400

    Streamlined code to have beforeEach

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,16 +1,34 @@
 /* eslint-disable testing-library/no-node-access */
 // src/__tests__/App.test.js
 
-import { render } from '@testing-library/react';
+/*import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('<App /> component', () => {
     test('renders list of events', () => {
-        const AppDOM = render(<App />).container.firstChild;
-        expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+        render(<App />);
+        expect(screen.getByTestId('event-list')).toBeInTheDocument();
       });
     test('render CitySearch', () => {
-        const AppDOM = render().container.firstChild;
-        expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
+        render(<App />);
+        expect(screen.getByTestId('city-search')).toBeInTheDocument();
     });
+});*/
+
+import { render } from '@testing-library/react';
+import App from '../App';
+
+describe('<App /> component', () => {
+  let AppDOM;
+  beforeEach(() => {
+    AppDOM = render(<App />).container.firstChild;
+  })
+
+  test('renders list of events', () => {
+    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+  });
+
+  test('render CitySearch', () => {
+    expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
+  });
 });
