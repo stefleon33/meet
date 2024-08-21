1503d210cedec98ecc19567a6f7d37097f8605dd
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-08-21 17:10:16 -0400

    test to render the number of events

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -18,4 +18,8 @@ describe('<App /> component', () => {
   test('render CitySearch', () => {
     expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
   });
+
+  test('render NumberOfEvents', () => {
+    expect(AppDOM.querySelector('#numberOfEvents')).toBeInTheDocument();
+  });
 });
