0f3f18b51302a8055635e05dba5f15fe62de5505
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-08-21 10:19:11 -0400

    Test to render event component

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -10,3 +10,8 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 //import userEvent from '@testing-library/user-event';
 
+
+      test('render event component', () => { 
+        expect(EventComponent).toBeTruthy();
+      });
+
