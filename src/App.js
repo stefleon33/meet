6070d7148142a25f9495231b34ceb5d3cb493aac
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-08-21 17:04:44 -0400

    Code for number of events in app.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,23 @@ import { getEvents } from './api';
 import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState([32]);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  }
+
+  useEffect(() => {
+    fetchData();
+  });
+
  return (
    <div className="App">
      <CitySearch />
-     <EventList />
-     <NumberOfEvents />
+     <EventList events={events}/>
+     <NumberOfEvents setCurrentNOE={setCurrentNOE}/>
    </div>
  );
 }
