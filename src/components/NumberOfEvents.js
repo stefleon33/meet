82b66b5edd6fda47f067151273f2170221e18456
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-08-21 16:12:11 -0400

    code for const

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -2,5 +2,20 @@
 
 import { useState } from "react"
 
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
+    const [number, setNumber] = useState(32)
+
+    const handleInputChanged = (e) => {
+        let value = e.target.value
+        setNumber(value)
+        let errorText;
+        if(value === isNaN || value.length <= 0) {
+            errorText = 'Please enter a valid number'
+        } else {
+            errorText = ''
+        }
+        setCurrentNOE(value)
+        setErrorAlert(errorText)
+    }
 
 export default NumberOfEvents;
