91519932acbd47f0b2912a96911085e7ce303e17
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-08-21 16:12:47 -0400

    return code written

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -17,5 +17,19 @@ const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
         setCurrentNOE(value)
         setErrorAlert(errorText)
     }
+    return (
+        <div id="numberOfEvents">
+            <label htmlFor="number" id="number">
+                Number of Events:
+            <input
+                type="text"
+                className="number"
+                value={number}
+                onChange={handleInputChanged}
+            />
+            </label>
+        </div>
+    )
+}
 
 export default NumberOfEvents;
