0acc3f70ac4d090857ca0ee7e8c1880b85f66f2f
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-08-21 10:12:50 -0400

    Create Event.test.js

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,5 @@
+/* eslint-disable testing-library/no-node-access */
+/* eslint-disable testing-library/prefer-presence-queries */
+/* eslint-disable testing-library/prefer-screen-queries */
+/* eslint-disable testing-library/no-render-in-setup */
+// src/__tests__/Event.test.js
