5a63f4cccf653f1317348dd0095447ada0737635
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-08-21 16:20:32 -0400

    Import for render and NumberOfEvents

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,5 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+/* eslint-disable testing-library/no-render-in-setup */
+import { render } from "@testing-library/react";
+import NumberOfEvents from "../components/NumberOfEvents";
+
