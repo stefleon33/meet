1629d0e19c02fbfea9fe3ffb7e0ec3a3f5c0a6ec
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-08-21 10:02:08 -0400

    Test renders the correct number of events

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/no-render-in-setup */
 /* eslint-disable testing-library/prefer-presence-queries */
 /* eslint-disable testing-library/prefer-screen-queries */
 /* eslint-disable testing-library/render-result-naming-convention */
@@ -6,6 +7,7 @@
 
 import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
+import { getEvents } from '../api';
 
 describe('<EventList /> component', () => {
     let EventListComponent;
@@ -16,10 +18,9 @@ describe('<EventList /> component', () => {
         expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
       });
      
-      test('renders correct number of events', () => {
-        EventListComponent.rerender(<EventList events={
-          [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
-        } />);
-        expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+      test('renders correct number of events', async () => {
+        const allEvents = await getEvents(); 
+        EventListComponent.rerender(<EventList events={allEvents} />);
+        expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
       });
      });
