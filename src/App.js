3214f01c53f4acaff0d1703c8b1b0b252c992713
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-03 13:49:55 -0400

    New state created: allLocations

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
-import { getEvents } from './api';
 import { useEffect, useState } from 'react';
+import { extractLocations, getEvents } from './api';
 
 const App = () => {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState([32]);
-
-  const fetchData = async () => {
-    const allEvents = await getEvents();
-    setEvents(allEvents.slice(0, currentNOE));
-  }
+  const [allLocations, setAllLocations] = useState([]);
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
+
  return (
    <div className="App">
-     <CitySearch />
+     <CitySearch allLocations={allLocations}/>
      <EventList events={events}/>
      <NumberOfEvents setCurrentNOE={setCurrentNOE}/>
    </div>
