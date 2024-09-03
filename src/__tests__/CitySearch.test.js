cc300386997e7f85f42f98d94b2ea5c8bd3ed05a
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-03 13:33:51 -0400

    Test renders suggestions list when the app is rendered

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -71,3 +71,21 @@ describe('<CitySearch /> component', () => {
     expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
   });
 });
+
+describe('<CitySearch /> integration', () => {
+  test('renders suggestions list when the app is rendered.', async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const CitySearchDOM = AppDOM.querySelector('#city-search');
+    const cityTextBox = within(CitySearchDOM).queryByRole('textbox');
+    await user.click(cityTextBox);
+
+    const allEvents = await getEvents();
+    const allLocations = extractLocations(allEvents);
+
+    const suggestionListItems = within(CitySearchDOM).queryAllByRole('listitem');
+    expect(suggestionListItems.length).toBe(allLocations.length + 1);
+ });
+});
