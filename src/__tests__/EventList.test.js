632d7c043607ba759b79246dc32955248675edc3
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-03 13:29:09 -0400

    integration test for rendering a list of 32 events

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -6,9 +6,10 @@
 
 // src/__tests__/EventList.test.js
 
-import { render } from '@testing-library/react';
+import { render, within, waitFor } from '@testing-library/react';
 import EventList from '../components/EventList';
 import { getEvents } from '../api';
+import App from "../App";
 
 describe('<EventList /> component', () => {
     let EventListComponent;
@@ -27,5 +28,13 @@ describe('<EventList /> component', () => {
      });
 
 describe('<EventList /> integration', () => {
-
+  test('renders a list of 32 events when the app is mounted and rendered', async () => {
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      expect(EventListItems.length).toBe(32);
+    });
+  });
 });
