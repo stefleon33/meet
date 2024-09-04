7c51c79c304e1b0dcdae1074f44c2c45e93d1230
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-04 13:20:49 -0400

    currentCity state

--- src/App.js
+++ src/App.js
@@ -11,20 +11,24 @@ const App = () => {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState([32]);
   const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [currentCity]);
 
   const fetchData = async () => {
     const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities" ?
+      allEvents :
+      allEvents.filter(event => event.location === currentCity)
     setEvents(allEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
   }
 
  return (
    <div className="App">
-     <CitySearch allLocations={allLocations}/>
+     <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
      <EventList events={events}/>
      <NumberOfEvents setCurrentNOE={setCurrentNOE}/>
    </div>
