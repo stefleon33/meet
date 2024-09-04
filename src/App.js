c382bc29e853983c5e49c1b69a840e663ffc47c6
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-04 13:44:11 -0400

    changed allEvents to filteredEvents

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
     const filteredEvents = currentCity === "See all cities" ?
       allEvents :
       allEvents.filter(event => event.location === currentCity)
-    setEvents(allEvents.slice(0, currentNOE));
+    setEvents(filteredEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
   }
 
