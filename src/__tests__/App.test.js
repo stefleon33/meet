1c02bcf861898ac0fb097bdb4aceec7e437e5f6c
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-04 12:36:43 -0400

    integration test for renders a list of events matching the city selected by the user

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -26,3 +26,28 @@ describe('<App /> component', () => {
     expect(AppDOM.querySelector('#numberOfEvents')).toBeInTheDocument();
   });
 });
+
+describe('<App /> integration', () => {
+  test('renders a list of events matching the city selected by the user', async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const CitySearchDOM = AppDOM.querySelector('#city-search');
+    const CitySearchInput = within(CitySearchDOM).queryByRole('textbox');
+
+    await user.type(CitySearchInput, "Berlin");
+    const berlinSuggestionItem = within(CitySearchDOM).queryByText('Berlin, Germany');
+    await user.click(berlinSuggestionItem);
+
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');   
+
+    const allEvents = await getEvents();
+    const berlinEvents = allEvents.filter(
+      event => event.location === 'Berlin, Germany'
+    );
+
+    expect(allRenderedEventItems.length).toBe(berlinEvents.length);
+  });
+});
