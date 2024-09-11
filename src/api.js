f84708745630c4e9b17ebe2b6533198f26b3b83c
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-11 12:16:52 -0400

    tokenCheck variable

--- src/api.js
+++ src/api.js
@@ -74,4 +74,22 @@ const getToken = async (code) => {
 
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
+
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        "https://4x2swxk0wi.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
 };
