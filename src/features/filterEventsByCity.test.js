64edcd6d5a212fd3c760f26c4b0a975ed3963f2b
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-16 16:49:29 -0400

    Scenario 2 test

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -3,6 +3,7 @@ import { loadFeature, defineFeature } from 'jest-cucumber';
 import { render, within, waitFor } from '@testing-library/react';
 import App from '../App';
 import { getEvents } from '../mock-data';
+import userEvent from '@testing-library/user-event';
 
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
@@ -31,16 +32,23 @@ defineFeature(feature, test => {
   });
 
     test('User should see a list of suggestions when they search for a city.', ({ given, when, then }) => {
-        given('the main page is open', () => {
+      let AppComponent;  
+      given('the main page is open', () => {
+        AppComponent = render(<App />);
+      });
 
-        });
+      let CitySearchDOM;
+      when('user starts typing in the city textbox', async () => {
+        const user = userEvent.setup();
+        const AppDOM = AppComponent.container.firstChild;
+        CitySearchDOM = AppDOM.querySelector('#city-search');
+        const citySearchInput = within(CitySearchDOM).queryByRole('textbox');  
+        await user.type(citySearchInput, "Berlin");
+      });
 
-        when('user starts typing in the city textbox', () => {
-
-        });
-
-        then('the user should recieve a list of cities (suggestions) that match what they’ve typed', () => {
-
+        then('the user should recieve a list of cities (suggestions) that match what they’ve typed', async () => {
+          const suggestionListItems = within(CitySearchDOM).queryAllByRole('listitem'); 
+          expect(suggestionListItems).toHaveLength(2);
         });
     });
 
