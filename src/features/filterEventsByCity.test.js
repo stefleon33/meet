8ec60c33c62deb5cb8d343f897b61d1bacb2849f
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-18 14:24:22 -0400

    Scenario 3 test

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -2,7 +2,7 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import { render, within, waitFor } from '@testing-library/react';
 import App from '../App';
-import { getEvents } from '../mock-data';
+import { getEvents } from '../api';
 import userEvent from '@testing-library/user-event';
 
 
@@ -53,24 +53,44 @@ defineFeature(feature, test => {
     });
 
     test('User can select a city from the suggested list.', ({ given, and, when, then }) => {
-        given('user was typing “Berlin” in the city textbox', () => {
+      let AppComponent;
+      let AppDOM; 
+      let CitySearchDOM;
+      let citySearchInput;  
+      given('user was typing “Berlin” in the city textbox', async () => {
+        AppComponent = render(<App />);
+        const user = userEvent.setup();
+        AppDOM = AppComponent.container.firstChild;
+        CitySearchDOM = AppDOM.querySelector('#city-search');
+        citySearchInput = within(CitySearchDOM).queryByRole('textbox');  
+        await user.type(citySearchInput, "Berlin");
+      });
 
-        });
+      let suggestionListItems;
+      and('the list of suggested cities is showing', () => {
+        suggestionListItems = within(CitySearchDOM).queryAllByRole('listitem'); 
+        expect(suggestionListItems).toHaveLength(2);
+      });
 
-        and('the list of suggested cities is showing', () => {
+      when('the user selects a city (e.g., “Berlin, Germany”) from the list', async () => {
+        const user = userEvent.setup();
+        await user.click(suggestionListItems[0]);
 
-        });
+      });
 
-        when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
+      then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
+        expect(citySearchInput.value).toBe('Berlin, Germany');
+      });
 
-        });
-
-        then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
-
-        });
-
-        and('the user should receive a list of upcoming events in that city', () => {
-
-        });
+      and('the user should receive a list of upcoming events in that city', async () => {
+        const EventListDOM = AppDOM.querySelector('#event-list');
+        const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+        const allEvents = await getEvents();
+    
+        // filtering the list of all events down to events located in Germany
+        // citySearchInput.value should have the value "Berlin, Germany" at this point
+        const berlinEvents = allEvents.filter(event => event.location === citySearchInput.value)
+        expect(EventListItems).toHaveLength(berlinEvents.length);
+      });
     });
 });
