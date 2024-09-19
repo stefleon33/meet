67eac3622cdc6145b17ebeb9d28a3f6a57108483
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-19 14:12:59 -0400

    Feature 2, scenario 2

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -15,4 +15,17 @@ describe('show/hide an event details', () => {
         expect(eventDetails).toBeNull();
         browser.close();
       });
+
+      test('User can expand an event to see its details', async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+    
+        await page.waitForSelector('.event');
+        await page.click('.event .details-btn');
+    
+        const eventDetails = await page.$('.event .details');
+        expect(eventDetails).toBeDefined();
+        browser.close();
+      });
 });
