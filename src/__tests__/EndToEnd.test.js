74092c7c01ee975ad0b245065500f5e6bd09354c
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-19 15:21:01 -0400

    Feature 2 Scenario 3 end to end test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -26,4 +26,10 @@ describe('show/hide an event details', () => {
         const eventDetails = await page.$('.event .details');
         expect(eventDetails).toBeDefined();
     });
+
+    test('User can collapse an event to hide details', async () => {
+        await page.click('.event .details-btn');
+        const eventDetails = await page.$('.event .details');
+        expect(eventDetails).toBeNull();
+      });
 });
