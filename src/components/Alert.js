8c693f0df5127f84dfaab7494790394deeab6b30
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-28 01:03:08 -0400

    InfoAlert subclass

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -33,3 +33,13 @@ class Alert extends Component {
     );
   }
 }
+  
+  class InfoAlert extends Alert {
+    constructor(props) {
+      super(props);
+      this.color = 'rgb(0, 0, 255)'; // blue
+      this.bgColor = 'rgb(220, 220, 255)'; // light blue
+    }
+  }
+  
+  export { InfoAlert };
