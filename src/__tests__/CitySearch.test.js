8745abadfee93fd35f9015186070f5678795304f
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-28 01:43:03 -0400

    dummy prop passed

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -15,7 +15,10 @@ import App from '../App';
 describe('<CitySearch /> component', () => {
   let CitySearchComponent;
   beforeEach(() => {
-    CitySearchComponent = render(<CitySearch allLocations={[]}/>);
+    CitySearchComponent = render(<CitySearch 
+      allLocations={[]}
+      setCurrentCity={() => { }}
+      setInfoAlert={() => { }}/>);
   });
 
   test('renders text input', () => {
@@ -41,7 +44,9 @@ describe('<CitySearch /> component', () => {
     const user = userEvent.setup();
     const allEvents = await getEvents();
     const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    CitySearchComponent.rerender(<CitySearch 
+      allLocations={allLocations} 
+      setInfoAlert={() => { }} />);
 
     // user types "Berlin" in city textbox
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -66,6 +71,7 @@ describe('<CitySearch /> component', () => {
     CitySearchComponent.rerender(<CitySearch
       allLocations={allLocations}
       setCurrentCity={() => { }}
+      setInfoAlert={() => { }}
     />);
     
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
