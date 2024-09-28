7d2b3549dfac2c227611762e3a9be3a168cde326
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-09-28 01:02:14 -0400

    InfoAlert created

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
+import { InfoAlert } from './components/Alert';
 
 const App = () => {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState([32]);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -29,7 +31,13 @@ const App = () => {
 
  return (
    <div className="App">
-     <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+     <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+      </div>
+     <CitySearch 
+      allLocations={allLocations} 
+      setCurrentCity={setCurrentCity} 
+      setInfoAlert={setInfoAlert} />
      <NumberOfEvents setCurrentNOE={setCurrentNOE}/>
      <EventList events={events}/>
    </div>
