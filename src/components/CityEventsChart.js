cb0659fffb4d00cad74544ac7da5d930b14e15e2
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-07 16:08:35 -0400

    CityEventsChart initial commit

---
+++ src/components/CityEventsChart.js
@@ -0,0 +1,30 @@
+// src/components/CityEventsChart.js
+
+import { useState, useEffect } from 'react';
+import {
+  ScatterChart,
+  Scatter,
+  XAxis, YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer
+} from 'recharts';
+
+const CityEventsChart = ({ allLocations, events }) => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      setData(getData());
+    }, [`${events}`]); 
+
+  const getData = () => {
+    const data = allLocations.map((location) => {
+    const count = events.filter((event) => event.location === location).length
+      const city = location.split(', ')[0]
+      return { city, count };
+    })
+    return data;
+  };
+}
+
+export default CityEventsChart;
