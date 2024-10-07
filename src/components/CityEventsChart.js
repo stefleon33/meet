0ca32b2568546f41b2a9bb4ddb461f027adfc272
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-07 16:44:57 -0400

    Update CityEventsChart.js

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -16,7 +16,7 @@ const CityEventsChart = ({ allLocations, events }) => {
 
   useEffect(() => {
     setData(getData());
-  }, [`${data}`]);
+  }, [`${events}`]);
 
   const getData = () => {
     const data = allLocations.map((location) => {
