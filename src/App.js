6db447665ecda22fd7b12089a3ea9f78ba191abc
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-07 11:04:02 -0400

    Fixed allEvents error

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
     const allEvents = await getEvents();
     const filteredEvents = currentCity === "See all cities" ?
       allEvents :
-      allEvents.filter(event => event.location === currentCity)
+      allEvents.filter(event => event.location === currentCity);
     const currentEvents = await filteredEvents.slice(0, currentNOE)
     setEvents(currentEvents);
     setAllLocations(extractLocations(allEvents));
