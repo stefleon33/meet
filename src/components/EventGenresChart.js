dea01058ee640b970f07beacc553571768a85beb
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-08 10:05:31 -0400

    imports for EventGenresChart

---
+++ src/components/EventGenresChart.js
@@ -0,0 +1,11 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+// src/components/EventsGenresChart.js
+
+import { useState, useEffect } from 'react';
+import {
+  PieChart, 
+  Pie, 
+  Cell,
+  Legend,
+  ResponsiveContainer
+} from 'recharts';
